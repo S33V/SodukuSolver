e2ac519b1630a05bba6920f482990010808510dd
Severiano Badajoz <sevebadajoz@gmail.com>
2018-05-03 13:33:11 -0700

    implement demo

--- src/Demo.java
+++ src/Demo.java
@@ -1,2 +1,7 @@
 public class Demo {
+    public static void main(String[] args) {
+        SudokuBoard gameboard = new SudokuBoard();
+        BoardCreater.createBoard(gameboard);
+        System.out.println(gameboard);
+    }
 }
