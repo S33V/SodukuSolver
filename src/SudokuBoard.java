1835a10e8c769e5b9e107ef14f3a954776e8d03f
Severiano Badajoz <sevebadajoz@gmail.com>
2018-05-03 13:17:49 -0700

    Implement basic functions

--- src/SudokuBoard.java
+++ src/SudokuBoard.java
@@ -1,2 +1,22 @@
 public class SudokuBoard {
+    int[][]board;
+     public SudokuBoard() {
+         board = new int[9][9];
+     }
+
+     public void editSquare(int col, int row, int value) {
+         board[row][col] = value;
+     }
+
+     public String toString() {
+         StringBuffer sb = new StringBuffer();
+         sb.append("--------------------\n");
+         for (int i = 0; i < board.length; i++) {
+             for(int j = 0; j < board[i].length; ++j){
+                 sb.append("|").append(board[i][j]).append("|");
+             }
+             sb.append("--------------------\n");
+         }
+         return sb.toString();
+     }
 }
