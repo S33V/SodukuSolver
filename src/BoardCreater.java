2650a64f4cd12b4d59a6692253ec0922f4eb619e
Severiano Badajoz <sevebadajoz@gmail.com>
2018-07-11 11:17:42 -0700

    Tweak algorithm

--- src/BoardCreater.java
+++ src/BoardCreater.java
@@ -5,13 +5,13 @@ public class BoardCreater {
         int[][] rows = randomRowCreator();
 //        int[][] rows = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-        for (int row = 0; row < 3; row++) { // Row
-            for (int blockRow = 0; blockRow < 3; blockRow++) { // blockRow
+        for (int rowBlock = 0; rowBlock < 3; rowBlock++) { // rowBlock
+            for (int row = 0; row < 3; row++) { // row
                 for (int trio = 0; trio < 3; trio++) { // Trio
                     for (int boardElement = 0; boardElement < 3; boardElement++) { // Individual square
-                        board.editSquare(trio * 3 + boardElement,
-                                blockRow * 3 + row,
-                                rows[(trio + row * 5) % 3][(boardElement + blockRow * 5) % 3]);
+                        board.editSquare(rowBlock * 3 + row,
+                                trio * 3 + boardElement,
+                                rows[(row * 5 + trio) % 3][(rowBlock * 5 + boardElement) % 3]);
                     }
                 }
             }
